9945871515000dbfa225484b51ad329e8dd1c6fc
Joe Njogu <joenjogu97@gmail.com>
2020-02-07 23:13:40 +0300

    Fixed floating action button - hidden in other fragments Fixed tracklist - layout padding, margins and nested linearlayout constraint

--- app/src/main/java/com/example/jpt_demo/MainActivity.kt
+++ app/src/main/java/com/example/jpt_demo/MainActivity.kt
@@ -16,6 +16,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
         toolbar.setTitle("Jumia Price Tracker")
         setSupportActionBar(toolbar)
 
